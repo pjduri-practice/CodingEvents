b2ee73946956ab40930fed8e7c7ec36cc39e18f8
Your Name <108307350+pjduri@users.noreply.github.com>
2023-04-06 12:16:28 -0500

    syncing

---
+++ CodingEvents/Controllers/EventCategoryController.cs
@@ -0,0 +1,23 @@
+﻿using CodingEvents.Data;
+using CodingEvents.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext context;
+
+        public EventCategoryController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<EventCategory> categories = context.Categories.ToList();
+
+            return View();
+        }
+    }
+}
---
+++ CodingEvents/Models/EventCategory.cs
@@ -0,0 +1,34 @@
+﻿namespace CodingEvents.Models
+{
+    public class EventCategory
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public EventCategory()
+        {
+        }
+
+        public EventCategory(string? name) : this()
+        {
+            Name = name;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is EventCategory category &&
+                   Id == category.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public override string? ToString()
+        {
+            return Name;
+        }
+
+    }
+}
