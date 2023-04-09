0648486f2169703f9ca5bbad2738cbac2adada46
Your Name <108307350+pjduri@users.noreply.github.com>
2023-04-09 12:33:02 -0500

    syncing

--- CodingEvents/Controllers/EventCategoryController.cs
+++ CodingEvents/Controllers/EventCategoryController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using CodingEvents.Data;
 using CodingEvents.Models;
+using CodingEvents.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -24,6 +26,33 @@ namespace CodingEvents.Controllers
         {
             List<EventCategory> categories = context.Categories.ToList();
             return View(categories);
+        }
+
+        // GET: /<controller>/<method>
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel();
+
+            return View(addEventCategoryViewModel);
+        }
+
+        // POST: /<controller>//eventcategory
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory newEventCategory = new EventCategory
+                {
+                    Name = addEventCategoryViewModel.Name
+                };
+
+                context.Categories.Add(newEventCategory);
+                context.SaveChanges();
+
+                return Redirect("/eventcategory");
+            }
+
+            return View();
         }
 
     }
