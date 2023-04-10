8d363b340ac62f7b7aa7579403c2ebb957c419cb
Your Name <108307350+pjduri@users.noreply.github.com>
2023-04-09 21:54:55 -0500

    added eventtag viewmodel

---
+++ CodingEvents/ViewModels/AddEventTagViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CodingEvents.Models;
+using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CodingEvents.ViewModels
+{
+    public class AddEventTagViewModel
+    {
+        public int EventId { get; set; }
+        public Event? Event { get; set; }
+        public int TagId { get; set; }
+        public List<SelectListItem>? Tags { get; set; }
+
+        public AddEventTagViewModel(Event theEvent, List<Tag> possibleTags)
+        {
+            Tags = new List<SelectListItem>();
+            foreach (var tag in possibleTags)
+            {
+                Tags.Add(new SelectListItem
+                {
+                    Value = tag.Id.ToString(),
+                    Text = tag.Name
+                });
+            }
+            Event = theEvent;
+        }
+
+        public AddEventTagViewModel()
+        {
+        }
+    }
+}
