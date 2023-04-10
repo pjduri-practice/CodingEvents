53850e4fbb3eaed0d0a62609197dc3bb89b4c7ec
Your Name <108307350+pjduri@users.noreply.github.com>
2023-04-09 20:42:40 -0500

    added event detail view, modelview and controller action method

--- CodingEvents/ViewModels/AddEventViewModel.cs
+++ CodingEvents/ViewModels/AddEventViewModel.cs
@@ -39,7 +39,6 @@ namespace CodingEvents.ViewModels
 
         public AddEventViewModel () 
         {
-            
         }
 
     }
---
+++ CodingEvents/ViewModels/EventDetailViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace CodingEvents.ViewModels
+{
+    public class EventDetailViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ContactEmail { get; set; }
+        public string CategoryName { get; set; }
+
+        public EventDetailViewModel(EventDetailViewModel theEvent) 
+        {
+            Name = theEvent.Name;
+            Description = theEvent.Description;
+            ContactEmail = theEvent.ContactEmail;
+            CategoryName = theEvent.Category.Name;
+        }
+    }
+}
