b925626a573a82ffbe9d2399c0646fb57b897e43
Your Name <108307350+pjduri@users.noreply.github.com>
2023-04-09 22:17:55 -0500

    added tags and add tag link to event detail view

--- CodingEvents/ViewModels/EventDetailViewModel.cs
+++ CodingEvents/ViewModels/EventDetailViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CodingEvents.Models;
+using System.Diagnostics.Tracing;
 
 namespace CodingEvents.ViewModels
 {
@@ -9,6 +10,8 @@ namespace CodingEvents.ViewModels
         public string? Description { get; set; }
         public string? ContactEmail { get; set; }
         public string? CategoryName { get; set; }
+        public string? TagText { get; set; }
+
 
         public EventDetailViewModel(Event theEvent) 
         {
@@ -17,6 +20,18 @@ namespace CodingEvents.ViewModels
             Description = theEvent.Description;
             ContactEmail = theEvent.ContactEmail;
             CategoryName = theEvent.Category.Name;
+
+            TagText = "";
+            List<Tag> evtTags = theEvent.Tags.ToList();
+
+            for (int i = 0; i < evtTags.Count; i++)
+            {
+                TagText += ($"#{evtTags[i].Name}");
+                if (i < evtTags.Count - 1)
+                {
+                    TagText += ", ";
+                }
+            }
         }
     }
 }
