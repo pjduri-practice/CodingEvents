7464a0fba2ee82a342c27aad63a0189561695c5a
Your Name <108307350+pjduri@users.noreply.github.com>
2023-04-16 19:05:00 -0500

    set EventDbContext to extend IdentityDbContext

--- CodingEvents/Data/EventDbContext.cs
+++ CodingEvents/Data/EventDbContext.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using CodingEvents.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting;
 
 namespace CodingEvents.Data
 {
-    public class EventDbContext : DbContext
+    public class EventDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
     {
         public DbSet<Event> Events { get; set; }
 
@@ -27,6 +29,8 @@ namespace CodingEvents.Data
                 .HasMany(t => t.Tags)
                 .WithMany(t => t.Events)
                 .UsingEntity(j => j.ToTable("EventTags"));
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
